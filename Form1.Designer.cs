a18cc377897e036d287762b51fec4eda76fa2a2d
IngrithR14 <nenita.yr34@gmail.com>
2025-03-14 20:13:18 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,351 @@
+﻿namespace Montecarlo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.lblR1 = new System.Windows.Forms.Label();
+            this.lblR2 = new System.Windows.Forms.Label();
+            this.lblIteration = new System.Windows.Forms.Label();
+            this.txtIteration = new System.Windows.Forms.TextBox();
+            this.lblM = new System.Windows.Forms.Label();
+            this.txtM = new System.Windows.Forms.TextBox();
+            this.lblB = new System.Windows.Forms.Label();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.lblA = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.lblSeed = new System.Windows.Forms.Label();
+            this.dgvResults = new System.Windows.Forms.DataGridView();
+            this.btnProcesar = new System.Windows.Forms.Button();
+            this.txtSeed = new System.Windows.Forms.TextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblTiempo = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResults)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(921, 577);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.chart1);
+            this.tabPage1.Controls.Add(this.lblR1);
+            this.tabPage1.Controls.Add(this.lblR2);
+            this.tabPage1.Controls.Add(this.lblIteration);
+            this.tabPage1.Controls.Add(this.txtIteration);
+            this.tabPage1.Controls.Add(this.lblM);
+            this.tabPage1.Controls.Add(this.txtM);
+            this.tabPage1.Controls.Add(this.lblB);
+            this.tabPage1.Controls.Add(this.txtB);
+            this.tabPage1.Controls.Add(this.lblA);
+            this.tabPage1.Controls.Add(this.txtA);
+            this.tabPage1.Controls.Add(this.lblSeed);
+            this.tabPage1.Controls.Add(this.dgvResults);
+            this.tabPage1.Controls.Add(this.btnProcesar);
+            this.tabPage1.Controls.Add(this.txtSeed);
+            this.tabPage1.Controls.Add(this.lblTitle);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(913, 544);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // lblR1
+            // 
+            this.lblR1.AutoSize = true;
+            this.lblR1.Location = new System.Drawing.Point(150, 356);
+            this.lblR1.Name = "lblR1";
+            this.lblR1.Size = new System.Drawing.Size(54, 20);
+            this.lblR1.TabIndex = 16;
+            this.lblR1.Text = "labelR";
+            // 
+            // lblR2
+            // 
+            this.lblR2.AutoSize = true;
+            this.lblR2.Location = new System.Drawing.Point(18, 356);
+            this.lblR2.Name = "lblR2";
+            this.lblR2.Size = new System.Drawing.Size(45, 20);
+            this.lblR2.TabIndex = 15;
+            this.lblR2.Text = "lblR2";
+            // 
+            // lblIteration
+            // 
+            this.lblIteration.AutoSize = true;
+            this.lblIteration.Location = new System.Drawing.Point(18, 245);
+            this.lblIteration.Name = "lblIteration";
+            this.lblIteration.Size = new System.Drawing.Size(88, 20);
+            this.lblIteration.TabIndex = 14;
+            this.lblIteration.Text = "Iteraciones";
+            // 
+            // txtIteration
+            // 
+            this.txtIteration.Location = new System.Drawing.Point(112, 245);
+            this.txtIteration.Name = "txtIteration";
+            this.txtIteration.Size = new System.Drawing.Size(118, 26);
+            this.txtIteration.TabIndex = 13;
+            // 
+            // lblM
+            // 
+            this.lblM.AutoSize = true;
+            this.lblM.Location = new System.Drawing.Point(18, 205);
+            this.lblM.Name = "lblM";
+            this.lblM.Size = new System.Drawing.Size(22, 20);
+            this.lblM.TabIndex = 10;
+            this.lblM.Text = "m";
+            // 
+            // txtM
+            // 
+            this.txtM.Location = new System.Drawing.Point(112, 202);
+            this.txtM.Name = "txtM";
+            this.txtM.Size = new System.Drawing.Size(118, 26);
+            this.txtM.TabIndex = 9;
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Location = new System.Drawing.Point(18, 159);
+            this.lblB.Name = "lblB";
+            this.lblB.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblB.Size = new System.Drawing.Size(18, 20);
+            this.lblB.TabIndex = 8;
+            this.lblB.Text = "b";
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(112, 159);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(118, 26);
+            this.txtB.TabIndex = 7;
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(18, 120);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(18, 20);
+            this.lblA.TabIndex = 6;
+            this.lblA.Text = "a";
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(112, 117);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(118, 26);
+            this.txtA.TabIndex = 5;
+            // 
+            // lblSeed
+            // 
+            this.lblSeed.AutoSize = true;
+            this.lblSeed.Location = new System.Drawing.Point(18, 79);
+            this.lblSeed.Name = "lblSeed";
+            this.lblSeed.Size = new System.Drawing.Size(60, 20);
+            this.lblSeed.TabIndex = 4;
+            this.lblSeed.Text = "Semilla";
+            // 
+            // dgvResults
+            // 
+            this.dgvResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResults.Location = new System.Drawing.Point(332, 25);
+            this.dgvResults.Name = "dgvResults";
+            this.dgvResults.RowHeadersWidth = 62;
+            this.dgvResults.RowTemplate.Height = 28;
+            this.dgvResults.Size = new System.Drawing.Size(479, 203);
+            this.dgvResults.TabIndex = 3;
+            // 
+            // btnProcesar
+            // 
+            this.btnProcesar.Location = new System.Drawing.Point(68, 300);
+            this.btnProcesar.Name = "btnProcesar";
+            this.btnProcesar.Size = new System.Drawing.Size(126, 37);
+            this.btnProcesar.TabIndex = 2;
+            this.btnProcesar.Text = "Presentar";
+            this.btnProcesar.UseVisualStyleBackColor = true;
+            this.btnProcesar.Click += new System.EventHandler(this.btnProcesar_Click);
+            // 
+            // txtSeed
+            // 
+            this.txtSeed.Location = new System.Drawing.Point(112, 73);
+            this.txtSeed.Name = "txtSeed";
+            this.txtSeed.Size = new System.Drawing.Size(118, 26);
+            this.txtSeed.TabIndex = 1;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(64, 20);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(146, 20);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Metodo Montecarlo";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.lblTiempo);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.chart2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(913, 544);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(332, 263);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(492, 243);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(35, 21);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series2.Legend = "Legend1";
+            series2.MarkerSize = 2;
+            series2.Name = "Series1";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "Series2";
+            this.chart2.Series.Add(series2);
+            this.chart2.Series.Add(series3);
+            this.chart2.Size = new System.Drawing.Size(641, 469);
+            this.chart2.TabIndex = 0;
+            this.chart2.Text = "chart2";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(705, 177);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(154, 90);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Graficar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblTiempo
+            // 
+            this.lblTiempo.AutoSize = true;
+            this.lblTiempo.Location = new System.Drawing.Point(761, 338);
+            this.lblTiempo.Name = "lblTiempo";
+            this.lblTiempo.Size = new System.Drawing.Size(51, 20);
+            this.lblTiempo.TabIndex = 3;
+            this.lblTiempo.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(945, 617);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResults)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dgvResults;
+        private System.Windows.Forms.Button btnProcesar;
+        private System.Windows.Forms.TextBox txtSeed;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblSeed;
+        private System.Windows.Forms.Label lblIteration;
+        private System.Windows.Forms.TextBox txtIteration;
+        private System.Windows.Forms.Label lblM;
+        private System.Windows.Forms.TextBox txtM;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.Label lblR1;
+        private System.Windows.Forms.Label lblR2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Label lblTiempo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Montecarlo
+{
+    public partial class Form1: Form
+    {
+        DataTable dataSet = new DataTable();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcesar_Click(object sender, EventArgs e)
+        {
+            int seed = Convert.ToInt32(txtSeed.Text);
+            int a = Convert.ToInt32(txtA.Text);
+            int b = Convert.ToInt32(txtB.Text);
+            int m = Convert.ToInt32(txtM.Text);
+            int iterations = Convert.ToInt32(txtIteration.Text);
+            //xn=(a*(xn-1)+b) mod m
+            //un=xn/m
+            float xn, un;
+            xn = (a * seed + b) % m;
+            un = xn / m;
+            lblR2.Text = xn.ToString();
+            lblR1.Text = un.ToString();
+            float[] arrXn = new float[iterations];
+            arrXn[0] = xn;
+            dataSet.Rows.Add(0, seed, arrXn[0] / m);
+            chart1.Series[0].Points.Clear();
+            for (int i = 1; i < iterations; i++)
+            {
+                arrXn[i]= (a * arrXn[i-1] + b) % m;
+                dataSet.Rows.Add(i, arrXn[i], arrXn[i]/m);
+                chart1.Series[0].Points.AddXY(i, arrXn[i]/m);
+            }
+           
+            dgvResults.DataSource = dataSet;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataSet.Columns.Add("Iteracion",typeof(int));
+            dataSet.Columns.Add("Xn", typeof(float));
+            dataSet.Columns.Add("Un", typeof(float));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            chart2.Series[0].Points.Clear();
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            chart2.Series[1].Points.AddXY(1, 1);
+            chart2.Series[1].Points.AddXY(1, 5);
+            chart2.Series[1].Points.AddXY(5, 5);
+            chart2.Series[1].Points.AddXY(5, 1);
+            chart2.Series[1].Points.AddXY(1, 1);
+            Random random = new Random();
+            for (int i = 0; i <100; i++)
+            {
+                float x = (float)(random.NextDouble()*(5-1)+1);
+                float y= (float)(random.NextDouble()*(5-1)+1);
+                chart2.Series[0].Points.AddXY(x,y);
+            }
+            watch.Stop();
+            lblTiempo.Text = watch.Elapsed.TotalSeconds.ToString();
+        }
+    }
+}
